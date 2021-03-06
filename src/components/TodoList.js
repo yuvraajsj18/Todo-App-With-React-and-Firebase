f5fd151ab51073fdc6e4379bd0043d996e886bfd
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2021-03-06 12:48:11 +0530

    add delete update error message

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoCheckmarkCircleOutline } from 'react-icons/io5'
 import { IoRemoveCircleOutline } from 'react-icons/io5'
 import noTodoIllustration from '../assets/images/no-todo.png'
 
 const TodoList = (props) => {
+  const [error, setError] = useState({});
+
+  const handleTodoComplete = async (todoId, isCompleted) => {
+    setError({});
+    try {
+      await props.onComplete(todoId, isCompleted);
+    }
+    catch (e) {
+      setError({message: "Oops! Something went wrong, please try again.", todoId:todoId});
+      console.log(e.message);
+    }
+  }
+
+  const handleTodoDelete = async (todoId) => {
+    setError({});
+    try {
+      await props.onDelete(todoId);
+    }
+    catch (e) {
+      setError({message: "Oops! Something went wrong, please try again.", todoId:todoId});
+      console.log(e.message);
+    }
+  }
 
   return (
       <div id="todos" className="my-8 mx-auto sm:max-w-md max-w-xs">
@@ -13,20 +36,26 @@ const TodoList = (props) => {
             {
               props.todos.map(todo => {
                 return (
-                  <li key={todo.id} 
+                  <div key={todo.id}>
+                  <li
                     className={`flex justify-between hover:shadow-md p-3 mb-4 border shadow ${todo.is_completed && "bg-green-50"} rounded-md`}>
                     {todo.task}
                     <div className="flex items-center">
-                      <button className="focus:outline-none" onClick={(e) => props.onComplete(todo.id, !todo.is_completed)}>
+                      <button className="focus:outline-none" onClick={(e) => handleTodoComplete(todo.id, !todo.is_completed)}>
                         <IoCheckmarkCircleOutline 
                           className="hover:text-green-600 inline text-2xl text-green-800 mr-1" 
                           />
                       </button>
-                      <button className="focus:outline-none" onClick={(e) => props.onDelete(todo.id)}>
+                      <button className="focus:outline-none" onClick={(e) => handleTodoDelete(todo.id)}>
                         <IoRemoveCircleOutline className="hover:text-red-600 inline text-2xl text-red-800 mr-1" />
                       </button>
                     </div>
                   </li>
+                  {
+                    error.message && error.todoId === todo.id &&
+                    <div className="text-red-600 -mt-4 mb-4 ml-1">{error.message}</div>
+                  }
+                  </div>
                 )
               })
             }
